fce1773ebbd47e0461db6209fcc0198be0e62c8e
Joelson Sartori <joelsonsartori@gmail.com>
2021-07-08 20:13:52 -0300

    feat: rota profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,24 @@
 import 'package:aloquei_app/screens/app.dart';
+import 'package:aloquei_app/screens/profile/profile.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Run());
+}
+
+class Run extends StatelessWidget {
+  Run({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Aloquei',
+      home: MyHomePage(),
+      routes: {
+        '/profile': (context) => ProfilePage(),
+      },
+      initialRoute: '/',
+    );
+  }
 }
