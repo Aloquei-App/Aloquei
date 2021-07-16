651037db67973ec943d519e13a0b359cdb4b7bff
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-07-16 20:56:26 -0300

    feat: filter version 1

--- lib/resources/offers/firestore_offers.dart
+++ lib/resources/offers/firestore_offers.dart
@@ -1,6 +1,7 @@
-import 'package:aloquei_app/core/models/house_offer_model.dart';
-import 'package:aloquei_app/core/models/interest_offer_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../../core/models/house_offer_model.dart';
+import '../../core/models/interest_offer_model.dart';
 
 class OffersRepository {
   FirebaseFirestore _firestoreInstance;
@@ -46,5 +47,47 @@ class OffersRepository {
   Future<bool> removeOffer(String docId) async {
     await _firestoreInstance.collection('offers').doc(docId).delete();
     return true;
+  }
+
+  Future<List<InterestModel>> getInterestsFiltered(
+      String estadoSigla, String cidade) async {
+    List<InterestModel> list = [];
+    try {
+      QuerySnapshot snapshot = await _firestoreInstance
+          .collection('offers')
+          .where('type', isEqualTo: 0)
+          .where('state', isEqualTo: estadoSigla)
+          .where('city', isEqualTo: cidade)
+          .get();
+      snapshot.docs.forEach((element) {
+        list.add(InterestModel.fromJson(element));
+      });
+    } catch (e, stack) {
+      print(e);
+      print(stack);
+      throw e;
+    }
+    return list;
+  }
+
+  Future<List<HouseOfferModel>> getHousesFiltered(
+      String estadoSigla, String cidade) async {
+    List<HouseOfferModel> list = [];
+    try {
+      QuerySnapshot snapshot = await _firestoreInstance
+          .collection('offers')
+          .where('type', isEqualTo: 1)
+          .where('state', isEqualTo: estadoSigla)
+          .where('city', isEqualTo: cidade)
+          .get();
+      snapshot.docs.forEach((element) {
+        list.add(HouseOfferModel.fromJson(element));
+      });
+    } catch (e, stack) {
+      print(e);
+      print(stack);
+      throw e;
+    }
+    return list;
   }
 }
