3fae182b4b8d8ca1a1ff72f7e030762c87930c41
Joelson Sartori <joelsonsartori@gmail.com>
2021-07-20 21:06:19 -0300

    feat: cor de fundo

--- lib/screens/core/colors.dart
+++ lib/screens/core/colors.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 //Cor exemplo - ainda a ser definida a identidade visual
 const themeColor = Colors.green;
+const backgroundColor = Colors.grey;
 const redAirbnb = Color(0xFFe61d53);
