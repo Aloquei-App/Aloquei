9f3c96dd44eaf7b1c6ba755f4e5248f02a100843
Nicolas-Daltrozo <nicolasdaltrozo@hotmail.com>
2021-07-30 19:00:54 -0300

    Homescreen

--- lib/screens/explore/explore.dart
+++ lib/screens/explore/explore.dart
@@ -22,7 +22,8 @@ class _ExplorePageState extends State<ExplorePage> {
     super.initState();
   }
   Widget build(BuildContext context) {
-    return Scaffold(
+    return SafeArea(child:
+      Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         backgroundColor: Color.fromRGBO(255, 255, 255, _opc),
@@ -60,7 +61,8 @@ class _ExplorePageState extends State<ExplorePage> {
           ),
         ),
       ),
-      body: SingleChildScrollView(
+      body:
+      SingleChildScrollView(
         controller: _scrollController,
         child: Column(
           children: <Widget>[
@@ -70,7 +72,7 @@ class _ExplorePageState extends State<ExplorePage> {
               decoration: BoxDecoration(
                 image: DecorationImage(
                   fit: BoxFit.cover,
-                  image: AssetImage('assets/images/backgr.jpg')
+                  image: NetworkImage('https://www.10wallpaper.com/wallpaper/medium/1512/Sunset_mountain_lake-HDR_Photography_Wallpaper_medium.jpg'),
                 ),
               ),
               padding: EdgeInsets.all(20),
@@ -130,42 +132,119 @@ class _ExplorePageState extends State<ExplorePage> {
               ),
             ),
 
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Container(
-                padding: EdgeInsets.only(left: 20),
-                height: 200,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      width: 100,
-                      color: Colors.red,
+            SizedBox(
+              height: 250,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 3,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.all(0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.transparent,
+                            shadowColor: Colors.transparent,
+                          ),
+                          onPressed: (){
+                            setState(() {
+                              if (index != 0){
+                                index = index*2;
+                              }
+                              print(index);
+                            });
+                          },
+                            child: Row(
+                              children: [
+                                Container(
+                                  height: 100,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                                    image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: NetworkImage('https://img.ibxk.com.br/2017/06/22/22100428046161.jpg?w=1200&h=675&mode=crop&scale=both'),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 150,
+                                  child: Container(
+                                    padding: EdgeInsets.only(left: 10),
+                                    child:Text('Nome da pessoa',
+                                      style: TextStyle(
+                                          fontSize: 15,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold
+                                      ),
+                                    ),
+                                  )
+                                )
+                              ],
+
+                            ),
+                          ),
+                        Padding(padding: EdgeInsets.all(10)),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.transparent,
+                            shadowColor: Colors.transparent,
+                          ),
+                          onPressed: (){
+                            setState(() {
+                              if (index != 0){
+                                index = index*2;
+                              }
+                              index = index+1;
+                              print(index);
+                            });
+                          },
+                          child: Row(
+                            children: [
+                              Container(
+                                height: 100,
+                                width: 100,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                                  image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: NetworkImage('https://img.ibxk.com.br/2017/06/22/22100428046161.jpg?w=1200&h=675&mode=crop&scale=both'),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                  width: 150,
+                                  child: Container(
+                                    padding: EdgeInsets.only(left: 10),
+                                    child:Text('Nome da pessoa',
+                                      style: TextStyle(
+                                          fontSize: 15,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold
+                                      ),
+                                    ),
+                                  )
+                              )
+                            ],
+
+                          ),
+                        ),
+                      ],
+
                     ),
-                    Container(
-                      width: 100,
-                      color: Colors.blue,
-                    ),
-                    Container(
-                      width: 100,
-                      color: Colors.green,
-                    ),
-                    Container(
-                      width: 100,
-                      color: Colors.pink,
-                    ), Container(
-                      width: 100,
-                      color: Colors.purple,
-                    ),
-                  ],
-                ),
+                  );
+                }
               ),
             ),
+
             Container(
-              padding: EdgeInsets.only(left: 20, top: 20),
+              padding: EdgeInsets.only(left: 20),
               child: Row(
                 children: <Widget>[
-                  Text('More em qualquer lugar',
+                  Text('More em uma casa',
                       style: TextStyle(
                         fontSize: 35,
                         fontWeight: FontWeight.bold,
@@ -174,40 +253,226 @@ class _ExplorePageState extends State<ExplorePage> {
                 ],
               ),
             ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
+
+            SizedBox(
+              height: 350,
+              child: ListView.builder(
+                  physics: ClampingScrollPhysics(),
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 4,
+                  itemBuilder: (context, index) {
+                    return ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.transparent,
+                          shadowColor: Colors.transparent,
+                        ),
+                        onPressed: (){
+                          setState(() {
+                            print(index);
+                          });
+                          },
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                                padding: EdgeInsets.only(top: 10, bottom: 10),
+                                child: Container(
+                                  height: 300,
+                                  width: 300,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                                    image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: NetworkImage('https://img.icons8.com/material-sharp/452/home.png'),
+                                    ),
+                                  ),
+                                )
+                            ),
+
+                            Container(
+                              child: Text('CASA AQUI',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                    );
+                  }
+              ),
+            ),
+
+
+            Padding(
+              padding: EdgeInsets.only(top:20,bottom: 20),
+              child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.all(0),
+                primary: Colors.transparent,
+                shadowColor: Colors.transparent,
+              ),
+              onPressed: (){},
               child: Container(
-                padding: EdgeInsets.all(20),
-                height: 200,
-                child: Row(
+                width: 390,
+                height: 500,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  border: Border.all(
+                    color: Colors.black,
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  image: DecorationImage(
+                    alignment: Alignment.bottomLeft,
+                    fit: BoxFit.fitWidth,
+                    image: NetworkImage('https://img.ibxk.com.br/2016/03/11/11152133282230.jpg?w=328'),
+                  ),
+                ),
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
                     Container(
-                      width: 100,
-                      color: Colors.red,
-                    ),
-                    Container(
-                      width: 100,
-                      color: Colors.blue,
-                    ),
-                    Container(
-                      width: 100,
-                      color: Colors.green,
-                    ),
-                    Container(
-                      width: 100,
-                      color: Colors.pink,
-                    ), Container(
-                      width: 100,
-                      color: Colors.purple,
+                      alignment: Alignment.center,
+                      child: Padding(
+                        padding: EdgeInsets.only(top: 60),
+                        child: Text('Clique aqui para fazer\no seu anuncio',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 35,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
                     ),
                   ],
                 ),
               ),
             ),
+            ),
+
+            Container(
+              padding: EdgeInsets.only(left: 20),
+              child: Row(
+                children: <Widget>[
+                  Text('More em uma republica',
+                      style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                      )
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(
+              height: 350,
+              child: ListView.builder(
+                  physics: ClampingScrollPhysics(),
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 3,
+                  itemBuilder: (context, index) {
+                    return ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.transparent,
+                        shadowColor: Colors.transparent,
+                      ),
+                      onPressed: (){
+                        setState(() {
+                          print(index);
+                        });
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                              padding: EdgeInsets.only(top: 10, bottom: 10),
+                              child: Container(
+                                height: 300,
+                                width: 300,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                                  image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: NetworkImage('https://i.pinimg.com/originals/7c/5d/74/7c5d7421753b2ef8221e778e77a89943.jpg'),
+                                  ),
+                                ),
+                              )
+                          ),
+
+                          Container(
+                            child: Text('REPUBLICA',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  }
+              ),
+            ),
+
+
+            Padding(
+              padding: EdgeInsets.only(top:20,bottom: 20),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(0),
+                  primary: Colors.transparent,
+                  shadowColor: Colors.transparent,
+                ),
+                onPressed: (){},
+                child: Container(
+                  width: 390,
+                  height: 500,
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    border: Border.all(
+                      color: Colors.black,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    image: DecorationImage(
+                      alignment: Alignment.bottomLeft,
+                      fit: BoxFit.fitWidth,
+                      image: NetworkImage('https://img.ibxk.com.br/2016/03/11/11152133282230.jpg?w=328'),
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        alignment: Alignment.center,
+                        child: Padding(
+                          padding: EdgeInsets.only(top: 60),
+                          child: Text('Clique aqui para fazer\no seu anuncio',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 35,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+
           ],
         ),
       ),
+    ),
     );
   }
 
