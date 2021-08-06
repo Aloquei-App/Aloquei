8403799b2b273764a746c2528fc8efac410ddb1f
= <cassianoss@live.com>
2021-08-05 22:29:17 -0300

    feat: rental_list criado e a definição dos componentes

---
+++ lib/screens/rental_list/rental_list.dart
@@ -0,0 +1,29 @@
+import 'package:aloquei_app/blocs/auth/auth_bloc.dart';
+import 'package:aloquei_app/screens/core/app_bars/default_app_bar.dart';
+import 'package:flutter/material.dart';
+
+class RentalList extends StatelessWidget {
+  const RentalList({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var widget;
+    return Scaffold(
+      appBar: DefaultAppBar(
+        action: () => widget.authBloc.add(
+          ExitEvent(),
+        ),
+      ),
+      body: Container(
+        child: ListView(
+          children: [
+            RentalTitle(),
+            RentalPresentCard(),
+            RentalSubtitle(),
+            RentalUnitCards(),
+          ],
+        ),
+      ),
+    );
+  }
+}
