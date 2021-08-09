b9292d4b31a65e0014d92a67c43cdea7aabe417a
Matheus Stallbaum Scheunemann <47986691+matheusscheu@users.noreply.github.com>
2021-08-09 16:57:46 -0300

    feat: alterações da tela

--- lib/screens/app.dart
+++ lib/screens/app.dart
@@ -1,23 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:responsive_framework/responsive_framework.dart';
+
 import '../blocs/auth/auth_bloc.dart';
 import 'core/snack_bar.dart';
 import 'explore/explore.dart';
 import 'home/home.dart';
 import 'inbox/inbox.dart';
+import 'login/login_page.dart';
 import 'profile/profile.dart';
+import 'search/search.dart';
 import 'signup/signup.dart';
 import 'splash/splash.dart';
 import 'trips/trips.dart';
 import 'wishlists/wishlists.dart';
 
 class Run extends StatelessWidget {
-  Run({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => AuthBloc()..add(AppStartedEvent()),
+      child: AppPage(),
+    );
+  }
+}
+
+class AppPage extends StatefulWidget {
+  @override
+  _AppPageState createState() => _AppPageState();
+}
+
+class _AppPageState extends State<AppPage> {
+  AuthBloc authBloc;
+
+  @override
+  void dispose() {
+    authBloc.close();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    authBloc = BlocProvider.of<AuthBloc>(context);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      supportedLocales: [const Locale('pt', 'BR')],
       builder: (context, widget) => ResponsiveWrapper.builder(
         BouncingScrollWrapper.builder(context, widget),
         maxWidth: 1200,
@@ -70,11 +106,13 @@ class Run extends StatelessWidget {
         ),
       ),
       routes: {
+        '/home': (context) => HomePage(),
         '/explore': (context) => ExplorePage(),
         '/wishlists': (context) => WishlistsPage(),
         '/trips': (context) => TripsPage(),
         '/inbox': (context) => InboxPage(),
         '/profile': (context) => ProfilePage(),
+        '/search': (context) => Search(),
       },
       initialRoute: '/',
     );
