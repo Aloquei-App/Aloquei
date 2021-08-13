608420e64ea9f7cccd3708354bf79bb9023e33ad
= <cassianoss@live.com>
2021-08-13 19:26:16 -0300

    save temp

--- lib/screens/rental_list/rental_list.dart
+++ lib/screens/rental_list/rental_list.dart
@@ -4,26 +4,29 @@ import 'package:flutter/material.dart';
 
 import 'components/rental_present_card.dart';
 import 'components/rental_title.dart';
+import 'components/rental_unit_card.dart';
 
 class RentalList extends StatelessWidget {
   const RentalList({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    var widget;
     return Scaffold(
-      appBar: DefaultAppBar(
-        action: () => widget.authBloc.add(
-          ExitEvent(),
-        ),
-      ),
       body: Container(
         child: ListView(
           children: [
             RentalTitle("Moradias"),
             RentalPresentCard(),
-            // RentalSubtitle(),
-            // RentalUnitCards(),
+            Padding(
+              padding: const EdgeInsets.only(top: 30),
+              child: RentalTitle("Suas Moradias"),
+            ),
+            RentalUnitCard(
+              "https://upload.wikimedia.org/wikipedia/commons/e/ed/112_Ocean_Avenue_%281973%29_2.jpg",
+              "Amytivylle", 
+              "Ronald DeFeo Jr.",
+              "09/fev/2020 - 15/fev/2020",
+            ),
           ],
         ),
       ),
